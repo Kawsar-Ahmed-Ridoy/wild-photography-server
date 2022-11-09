cf7b11bba2b3b0564602a60644e82f6fe4db8936
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-11-09 20:51:42 +0600

    mongodb multi data collection to single data collect function added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -27,7 +27,12 @@ async function run(){
             res.send(services);
         });
 
-        
+        app.get('/services/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
     }
     finally{
 
